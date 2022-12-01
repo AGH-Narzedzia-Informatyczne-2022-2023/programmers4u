ed6e415f0f7ee5dcff1363be8345d6b4c0861bf9
PiotrBAGH <115559381+PiotrBAGH@users.noreply.github.com>
2022-12-01 20:14:24 +0100

    Update save.py

--- save.py
+++ save.py
@@ -3,5 +3,5 @@ import sys
 from pygame.locals import *
 def input(self):
 	for zdarzenie in pygame.event.get():
-		elif zdarzenie.type == pygame.KEYDOWN and zdarzenie.key == pygame.K_z:
+		elif zdarzenie.type == pygame.KEYDOWN and zdarzenie.key == pygame.K_y:
             zapis()
