edddad0569e71c242580b5b6b5decff6f579f26d
soberyoda <115737563+soberyoda@users.noreply.github.com>
2022-12-01 20:22:29 +0100

    Create Ihavenoidea.py

---
+++ Ihavenoidea.py
@@ -0,0 +1,26 @@
+import pygame, sys
+from settings import *
+
+
+class Game:
+    def __init__(self):
+        # general setup
+
+        pygame.init()  # pygame initialization
+        self.screen = pygame.display.set_mode((WIDTH, HEIGHT))
+        pygame.display.set_caption("Dinosaur Adventure Game")  # window title
+        self.clock = pygame.time.Clock()  # create a clock
+
+    def run(self):
+        while True:  # event loop
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:  # closing pygame window
+                    pygame.quit()
+                    sys.exit()  # exit from python
+            pygame.display.update()
+            self.clock.tick(FPS)  # control frame rate
+
+
+if __random_change__ == "__main__":  # check if this is a main file
+    game = Game()  # instance of class
+    game.run()  # call method run of class Game()
